e2d590675adf1960aaebffa2d9870ce42a6e4379
Daniel Compton <desk@danielcompton.net>
2014-12-01 14:40:05 +1300

    Working point

--- src/cljs/game_of_life/core.cljs
+++ src/cljs/game_of_life/core.cljs
@@ -1,29 +1,57 @@
 (ns game-of-life.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [game-of-life.game :as game]))
 
-(def app-state (atom {:text "Hello dog Chestnut!"
-                      :grid [[0 1 2 3 4 5] [6 7 8 9 10] [11 12 13 14 15 16]]
-                      :columns 5
-                      :rows 5}))
+(enable-console-print!)
+
+(def app-state (atom {:size          5
+                      :board         [0 0 0 0 0
+                                      0 0 0 0 0
+                                      0 0 1 1 0
+                                      0 0 0 0 0
+                                      0 0 0 0 0]
+                      :click-counter 5}))
+
+(defn btn-view [cursor _]
+  (reify om/IRender
+    (render [_]
+      (dom/div nil
+               (dom/span nil (do (println (get cursor :click-counter))
+                                 (println cursor)
+                                 (str "Clicked " (get cursor :click-counter) " times")))
+               (dom/button
+                 #js {:onClick (fn [_]
+                                 #_(om/transact! cursor :click-counter inc)
+                                 (om/transact! cursor :board game/play))}
+                 "Play")))))
 
 (defn flex-item [cell]
   (om/component
-    (dom/div #js {:class "flex-item"}
+    (dom/div #js {:className (if (zero? cell) "flex-item" "flex-item alive")}
              cell)))
 
-(defn flex-column [rows]
+(defn flex-row [cells]
   (om/component
-    (dom/div #js {:class "column"}
-             (om/build-all flex-item rows))))
+    (apply dom/div #js {:className "row"}
+           (om/build-all flex-item cells))))
+
+(defn grid [app]
+  (reify
+    om/IRender
+    (render [_]
+      (apply dom/div #js {:className "flex-container"}
+             (let [size (app :size)
+                   start (app :board)]
+               (om/build-all flex-row (partition size start)))))))
 
 (defn app [app owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div #js {:class "flex-container"}
-               (om/build-all flex-column (:grid app))
-               ))))
+      (dom/div nil
+        (om/build grid app)
+        (om/build btn-view app)))))
 
 (defn main []
   (om/root
@@ -32,3 +60,48 @@
     {:target (. js/document (getElementById "app"))}))
 
 
+
+;(def app-state
+;  (atom
+;    {:widgets
+;     [{:my-number 16
+;       :text "I'm divisible by 2!"
+;       :grid [1 1 3 5]}
+;      {:my-number 23
+;       :text "I'm not divisible by 2!"}]}))
+;
+;(defmulti even-odd-widget (comp even? :my-number))
+;
+;(defmethod even-odd-widget true
+;  [props owner]
+;  (reify
+;    om/IRender
+;    (render [_]
+;      (dom/div nil
+;               (dom/h2 nil "Even Widget")
+;               (dom/p nil (:text props))
+;               (dom/p nil (pr-str  (:grid props)))))))
+;
+;(defmethod even-odd-widget false
+;  [props owner]
+;  (reify
+;    om/IRender
+;    (render [_]
+;      (dom/div nil
+;               (dom/h2 nil "Odd Widget")
+;               (dom/p nil (:text props))))))
+;
+;(defn app [props owner]
+;  (reify
+;    om/IRender
+;    (render [_]
+;      (apply dom/div nil
+;             (om/build-all even-odd-widget (:widgets props))))))
+
+
+
+(defn main []
+  (om/root
+    app
+    app-state
+    {:target (. js/document (getElementById "app"))}))
--- src/cljx/game_of_life/game.cljx
+++ src/cljx/game_of_life/game.cljx
@@ -4,3 +4,32 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(def start [0 0 0 0 0
+            0 0 0 0 0
+            0 1 1 1 0
+            0 0 0 0 0
+            0 0 0 0 0])
+
+(defn live-or-die [neighbor-count]
+  (case neighbor-count
+    2 1
+    3 1
+    0))
+
+(defn neighbors
+  "Returns the indexes of a cells neighbors in a square array of size `size`"
+  [size idx]
+  (let [keyseq [(inc 0) (dec size) size (inc size)]
+        neighbors (concat (map - keyseq) keyseq)]
+    (map (partial + idx) neighbors)))
+
+(defn play
+  ([board]
+    (play 5 board))
+  ([size board]
+    (vec (map-indexed (fn [idx _] (->> (neighbors size idx)
+                                       (select-keys board)
+                                       (vals)
+                                       (apply +)
+                                       (live-or-die))) board))))
